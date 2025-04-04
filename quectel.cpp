4be62c53f8ab5a74bf159968d9cb4de07538fc88
rahilshah22 <rahilshah4362@gmail.com>
2025-04-04 12:48:25 +0530

    second commit

--- quectel.cpp
+++ quectel.cpp
@@ -16,21 +16,4 @@ string QuectelToApp::getAppState(QuectelState state) {
     }
 }
 
-int main() {
-    int userChoice;
-    cout << "Enter a choice (0: SUCCEED, 1: UPDATE, 2: WRITEDONE, 3: SYNC_PENDING, 4: BACKUP, 5: CANCEL): ";
-    cin >> userChoice;
 
-    if (userChoice < 0 || userChoice > 5) {
-        cout << "Invalid choice. Please enter a number between 0 and 5.\n";
-        return 1;
-    }
-
-    QuectelToApp mapper;
-    QuectelState state = static_cast<QuectelState>(userChoice);
-
-    string appState = mapper.getAppState(state);
-    cout << "Mapped App State: " << appState << endl;
-
-    return 0;
-}
